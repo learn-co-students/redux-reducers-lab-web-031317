2f64e6da7eca72dda7ffd664505df42a1632bbe4
Sammy Steiner <sammysteiner@gmail.com>
2017-06-20 17:32:09 -0400

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,17 @@
-export default function ingredientReducer() {
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return [...state, action.payload]
+    case 'REMOVE_INGREDIENT':
+      let newState = []
+      state.forEach( item => {
+        if ( item !== action.payload ) {
+          newState.push(item)
+        }
+      })
+      return newState
+    default:
+      return state
 
+  }
 }
